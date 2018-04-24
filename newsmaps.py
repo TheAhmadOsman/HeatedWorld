2bc3d4d6abac4a15a300fa8d3023e27b43343be0
Ahmad Osman <ahmad.osman@luther.edu>
2018-04-24 00:01:53 -0500

    Flask routes

--- newsmaps.py
+++ newsmaps.py
@@ -39,84 +39,85 @@ def save_json(day, week):
 
 def save_json_map(week):
     logging.info("Ongoing...")
-    with open('map.json', 'w') as outfile:
+    with open('maps.json', 'w') as outfile:
         json.dump(week._countries, outfile)
 
 
-# def map_elements():
-#     logging.basicConfig(filename='newsmaps.log', level=logging.INFO,
-#                         format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
+def map_elements():
+    logging.basicConfig(filename='newsmaps.log', level=logging.INFO,
+                        format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
 
-#     logging.info("--------------------")
-#     logging.info("-------------")
-#     logging.info("Script starting...\n")
+    logging.info("--------------------")
+    logging.info("-------------")
+    logging.info("Script starting...\n")
 
-#     logging.info("Initiating client for the day's submssions...")
-#     # subreddit followed by day/week
-#     redditDay = Reddit("worldnews", "day")
-#     logging.info("Client initiated.")
+    logging.info("Initiating client for the day's submssions...")
+    # subreddit followed by day/week
+    redditDay = Reddit("worldnews", "day")
+    logging.info("Client initiated.")
 
-#     logging.info("Fetching...")
-#     redditDay.fetch(40)
-#     logging.info("Fetched.")
+    logging.info("Fetching...")
+    redditDay.fetch(40)
+    logging.info("Fetched.")
 
-#     logging.info("Getting context...")
-#     redditDay.get_context()
-#     logging.info("Context saved.")
+    logging.info("Getting context...")
+    redditDay.get_context()
+    logging.info("Context saved.")
 
-#     logging.info("Saving data to CSV...")
-#     redditDay.save()
-#     logging.info("Saved day's submissions.\n")
+    logging.info("Saving data to CSV...")
+    redditDay.save()
+    logging.info("Saved day's submissions.\n")
 
-#     logging.info("----------------------------------------------\n")
+    logging.info("----------------------------------------------\n")
 
-#     logging.info("Initiating client for the week's submssions...")
-#     # subreddit followed by day/week
-#     redditWeek = Reddit("worldnews", "week")
-#     logging.info("Client initiated.")
+    logging.info("Initiating client for the week's submssions...")
+    # subreddit followed by day/week
+    redditWeek = Reddit("worldnews", "week")
+    logging.info("Client initiated.")
 
-#     logging.info("Fetching...")
-#     redditWeek.fetch(100)
-#     logging.info("Fetched.")
+    logging.info("Fetching...")
+    redditWeek.fetch(100)
+    logging.info("Fetched.")
 
-#     logging.info("Getting context...")
-#     redditWeek.get_context()
-#     logging.info("Context saved.")
+    logging.info("Getting context...")
+    redditWeek.get_context()
+    logging.info("Context saved.")
 
-#     logging.info("Saving data to CSV...")
-#     redditWeek.save()
-#     logging.info("Saved week's submissions.\n")
+    logging.info("Saving data to CSV...")
+    redditWeek.save()
+    logging.info("Saved week's submissions.\n")
 
-#     logging.info("----------------------------------------------\n")
+    logging.info("----------------------------------------------\n")
 
-#     logging.info("Saving to JSON file...")
-#     save_json(redditDay, redditWeek)
-#     logging.info(("Saved JSON file.\n"))
+    logging.info("Saving to JSON file...")
+    save_json(redditDay, redditWeek)
+    logging.info(("Saved JSON file.\n"))
 
-#     ###
-#     save_json_map(redditWeek)
+    ###
+    save_json_map(redditWeek)
 
-#     logging.info("Script finished.")
-#     logging.info("-------------")
-#     logging.info("--------------------\n")
+    logging.info("Script finished.")
+    logging.info("-------------")
+    logging.info("--------------------\n")
 
 
-# map_elements()
-# scheduler = BackgroundScheduler()
-# scheduler.start()
-# scheduler.add_job(
-#     func=map_elements,
-#     trigger=IntervalTrigger(weeks=1),
-#     id='getting_elements',
-#     name='getting elements',
-#     replace_existing=True)
-# # Shut down the scheduler when exiting the app
-# atexit.register(lambda: scheduler.shutdown())
+map_elements()
+scheduler = BackgroundScheduler()
+scheduler.start()
+scheduler.add_job(
+    func=map_elements,
+    trigger=IntervalTrigger(weeks=1),
+    id='getting_elements',
+    name='getting elements',
+    replace_existing=True)
+# Shut down the scheduler when exiting the app
+atexit.register(lambda: scheduler.shutdown())
 
 
 @app.route('/')
 def homepage():
     return render_template("index.html")
+
 
 @app.route('/data')
 def fetchData():
@@ -125,13 +126,13 @@ def fetchData():
     f.close()
     return content
 
-@app.route('/map')
+
+@app.route('/maps')
 def fetchMap():
     f = open("maps.json", "r")
     content = f.read()
     f.close()
     return content
-
 
 
 if __name__ == '__main__':
