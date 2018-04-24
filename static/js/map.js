03650e2273088daadd7965676ea4d737b7005b80
Ahmad Osman <ahmad.osman@luther.edu>
2018-04-24 03:22:53 -0500

    Fixing JS warnings.

--- static/js/map.js
+++ static/js/map.js
@@ -3,25 +3,25 @@ function map() {
 
     // expected arrangment: {"countryName": {"fillColor": #colorcode,
     // "numberOfWhatever": num}}
-    let dataSet = {}
+    let dataSet = {};
 
-    let values = []
+    let values = [];
 
-    readJSON("/map")
-    readJSON("/data")
+    readJSON("/map");
+    readJSON("/data");
 
-    var valueDict = JSON.parse(localStorage.getItem("/map"))
+    var valueDict = JSON.parse(localStorage.getItem("/map"));
 
     //fetching values to determine min/max
     for (let item in valueDict) {
-        values.push(valueDict[item])
+        values.push(valueDict[item]);
     }
 
-    var minValue = Math.min(...values)
-    var maxValue = Math.max(...values)
+    var minValue = Math.min(...values);
+    var maxValue = Math.max(...values);
 
-    var minColor = "#FFFFB2"
-    var maxColor = "#BD0026"
+    var minColor = "#FFFFB2";
+    var maxColor = "#BD0026";
 
     // using d3 scale module to build a scale between dark green (light) and dark
     // red (heavy)
@@ -29,31 +29,31 @@ function map() {
         .scale
         .linear()
         .domain([minValue, maxValue])
-        .range([minColor, maxColor])
+        .range([minColor, maxColor]);
 
-    var halfWay = Math.round((maxValue - minValue) * .5)
-    var two = Math.round((maxValue - minValue) * .25)
-    var four = Math.round((maxValue - minValue) * .75)
+    var halfWay = Math.round((maxValue - minValue) * 0.5);
+    var two = Math.round((maxValue - minValue) * 0.25);
+    var four = Math.round((maxValue - minValue) * 0.75);
 
     //fetch country/value and popluate the dataSet
-    var fills = {}
+    var fills = {};
     for (let item in valueDict) {
-        let country = item
-        let val = valueDict[item]
+        let country = item;
+        let val = valueDict[item];
         if (country == "United States of America") {
-            country = "United States"
+            country = "United States";
         }
         dataSet[getIsoThree(country)] = {
             "fillColor": colorScale(val),
             "numberOfWhatever": val
-        }
+        };
     }
     for (let item in isoCountries) {
         if (getIsoThree(item) in dataSet) {} else {
             dataSet[getIsoThree(item)] = {
                 "fillColor": minColor,
                 "numberOfWhatever": 0
-            }
+            };
         }
     }
 
@@ -97,46 +97,46 @@ function map() {
                     /*                    let link = "popup.html#"
                           link = link + geography.properties.name
                           window.open(link, "_blank");*/
-                    let fullCountry = geography.properties.name
-                    let iso = getIsoThree(fullCountry)
-                    let popup = document.getElementById("popup")
-                    let close = document.getElementById("close")
-                    popup.style.display = "block"
+                    let fullCountry = geography.properties.name;
+                    let iso = getIsoThree(fullCountry);
+                    let popup = document.getElementById("popup");
+                    let close = document.getElementById("close");
+                    popup.style.display = "block";
                     close.onclick = function () {
-                        popup.style.display = "none"
-                    }
+                        popup.style.display = "none";
+                    };
                     window.onclick = function (event) {
                         if (event.target == popup) {
-                            popup.style.display == "none"
+                            popup.style.display == "none"; // jshint ignore:line
                         }
-                    }
-                    let popupHead = document.getElementById("popupHead")
-                    let popupBody = document.getElementById("popupBody")
-                    let popupFoot = document.getElementById("popupFoot")
-                    popupHead.innerHTML = "News from " + fullCountry
+                    };
+                    let popupHead = document.getElementById("popupHead");
+                    let popupBody = document.getElementById("popupBody");
+                    let popupFoot = document.getElementById("popupFoot");
+                    popupHead.innerHTML = "News from " + fullCountry;
                     //building the data dictionary from localStorage
-                    var dataDict = JSON.parse(localStorage.getItem("/data"))
-                    console.log(dataDict)
-                    popupString = ""
-                    var countryDict = dataDict[fullCountry]
-                    for (article in countryDict) {
-                        let title = countryDict["submission.title"]
-                        let link = countryDict["submission.url"]
-                        let score = countryDict["submission.score"]
-                        let comments = countryDict["submission.num_comments"]
+                    var dataDict = JSON.parse(localStorage.getItem("/data"));
+                    console.log(dataDict);
+                    popupString = "";
+                    var countryDict = dataDict[fullCountry];
+                    for (var article in countryDict) {
+                        let title = countryDict["submission.title"];
+                        let link = countryDict["submission.url"];
+                        let score = countryDict["submission.score"];
+                        let comments = countryDict["submission.num_comments"];
                         //Spaces in reddit
-                        let reddit = countryDict["submission.permalink"]
+                        let reddit = countryDict["submission.permalink"];
                         let photo = "https://img.thedailybeast.com/image/upload/c_crop,d_placeholder_euli9k,h_1440,w_" +
                                 "2560,x_0,y_0/dpr_2.0/c_limit,w_740/fl_lossy,q_auto/v1491846321/cheats/2017/02/02" +
                                 "/matthew-mcconaughey-time-to-embrace-trump/170202-mcconaughey-trump-bbc-cheat_df" +
-                                "ukwc"
-                        popupString = popupString + "<h5><b><a href=" + link + " target=_blank>" + title + "</a></b></h5><p><a href=" + reddit + " target=_blank> Number of comments: " + comments + "</a></p><p><a href=" + reddit + " target=_blank> Number of likes: " + score + "</a></p><img src=" + photo + " alt=Photo </img>"
+                                "ukwc";
+                        popupString = popupString + "<h5><b><a href=" + link + " target=_blank>" + title + "</a></b></h5><p><a href=" + reddit + " target=_blank> Number of comments: " + comments + "</a></p><p><a href=" + reddit + " target=_blank> Number of likes: " + score + "</a></p><img src=" + photo + " alt=Photo </img>";
                     }
                     popupBody.innerHTML = popupString;
 
                 });
         }
-    })
+    });
     map.legend({
         labels: {
             minValue: minValue,
@@ -145,7 +145,7 @@ function map() {
             four: four,
             maxValue: maxValue
         }
-    })
+    });
 
     window.addEventListener('resize', function () {
         map.resize();
